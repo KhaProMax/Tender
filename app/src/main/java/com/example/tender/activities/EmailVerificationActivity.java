7607fc29f34f4a8db3789e0f31a9783c3389de80
KhaProMax <khaqt@gmail.com>
2024-04-16 07:29:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tender/activities/EmailVerificationActivity.java
@@ -0,0 +1,16 @@
+package com.example.tender.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tender.R;
+
+public class EmailVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_verification);
+    }
+}
---
+++ app/src/main/java/com/example/tender/activities/MainActivity.java
@@ -0,0 +1,74 @@
+package com.thecode.tinderclone.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.example.tender.R;
+//import com.example.tender.Utils;
+import com.example.tender.adapters.ViewPagerAdapter;
+import com.example.tender.models.Profile;
+import com.example.tender.models.TinderCard;
+import com.example.tender.fragments.AccountFragment;
+import com.example.tender.fragments.ActivityFragment;
+import com.example.tender.fragments.ChatFragment;
+import com.example.tender.fragments.FeedFragment;
+import com.example.tender.fragments.SwipeViewFragment;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+
+    private Context mContext;
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        BottomNavigationView bnv = findViewById(R.id.bottom_navigation);
+
+        ArrayList<Fragment> fragList = new ArrayList<>();
+        fragList.add(new AccountFragment());
+        fragList.add(new SwipeViewFragment());
+        fragList.add(new ActivityFragment());
+        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(fragList, getSupportFragmentManager());
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setOffscreenPageLimit(3);
+        bnv.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.account:
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.fire:
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.chat:
+                viewPager.setCurrentItem(2);
+                break;
+        }
+        return true;
+    }
+}
