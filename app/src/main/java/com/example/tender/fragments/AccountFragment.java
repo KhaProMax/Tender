8dbca13791f1c49809249638ff655e1e82ac7a46
Hoang Luong <luonghoang2k2@gmail.com>
2024-06-30 14:02:41 +0700

    feat: Upload image to storage(firebase)

--- app/src/main/java/com/example/tender/fragments/AccountFragment.java
+++ app/src/main/java/com/example/tender/fragments/AccountFragment.java
@@ -2,6 +2,7 @@ package com.example.tender.fragments;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
@@ -22,20 +24,25 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.tender.R;
 import com.example.tender.activities.EditProfileActivity;
 import com.example.tender.activities.SplashscreenActivity;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.UUID;
 //import com.thecode.tinderclone.adapters.SliderAdapter;
 
 public class AccountFragment extends Fragment {
-
+    public static final int PICK_IMAGE_REQUEST = 1;
+    StorageReference storageReference;
     private ImageButton editBtn;
     // For test some funcs, modify this button later
     private ImageButton uploadBtn;
+    Uri image;
     View rootLayout;
 //    private SliderView sliderView;
 
     public AccountFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,6 +52,10 @@ public class AccountFragment extends Fragment {
 
         editBtn = rootLayout.findViewById(R.id.edit_button);
         uploadBtn = rootLayout.findViewById(R.id.upload_button);
+
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+
+
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,16 +65,46 @@ public class AccountFragment extends Fragment {
             }
         });
 
-        uploadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("TExt","Called");
-                NavHostFragment.findNavController(AccountFragment.this)
-                        .navigate(R.id.action_chatListFragment_to_chatBoxFragment);
-            }
-        });
+        uploadBtn.setOnClickListener(v -> openFile());
 
         return rootLayout;
     }
 
+    private void openFile() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && data != null && data.getData() != null) {
+            uploadFile(data.getData());
+            updateUser();
+        }
+    }
+
+    private void uploadFile(Uri imageUri) {
+        if (imageUri != null) {
+            StorageReference fileReference = storageReference.child("images/" + UUID.randomUUID().toString());
+
+            fileReference.putFile(imageUri)
+                    .addOnSuccessListener(taskSnapshot -> {
+                        Toast.makeText(getActivity(), "Upload successful", Toast.LENGTH_SHORT).show();
+                        // Call your method to display the uploaded image if needed
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
+        }
+        else {
+            Toast.makeText(getActivity(), "Please choose image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateUser() {
+        //From uid of user update imgUrl
+    }
 }
